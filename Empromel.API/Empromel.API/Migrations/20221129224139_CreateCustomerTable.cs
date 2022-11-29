9a1be06fed22d22b85545b9aa51ee16c23d9f05d
Gabriel Antonio Savi <gabriel.savi@clearsale.com.br>
2022-11-29 19:51:40 -0300

    Criando estrutura inicial da API

---
+++ Empromel.API/Empromel.API/Migrations/20221129224139_CreateCustomerTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Empromel.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCustomerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Cpf = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(1)", maxLength: 1, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Cep = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: true),
+                    Street = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    AddressNumber = table.Column<int>(type: "int", maxLength: 4, nullable: false),
+                    AddressSupplement = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Uf = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Cpf);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}

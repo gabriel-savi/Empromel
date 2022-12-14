78573dbe1221e16e0f1f673897cef8a6336130d2
JhonnyPonce <jhonnymaikom@hotmail.com>
2022-12-14 19:11:59 -0300

    Criação do model e context dos produtos

--- Empromel.API/Empromel.API/Data/Context/DataContext.cs
+++ Empromel.API/Empromel.API/Data/Context/DataContext.cs
@@ -10,5 +10,10 @@ namespace Empromel.API.Data.Context
         }
 
         public DbSet<Customer> Customers { get; set; }
+
+
+        public DbSet<Product> Products { get; set; }
+
+
     }
 }
---
+++ Empromel.API/Empromel.API/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Empromel.API.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(4)]
+        public string Quantity { get; set; }
+
+        [Required]
+        public DataType Lot { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(7)]
+        public string Price_Paid { get; set; }
+
+        [Required]
+        [MaxLength(7)]
+        public string Price_Charged { get; set; }
+
+    }
+}

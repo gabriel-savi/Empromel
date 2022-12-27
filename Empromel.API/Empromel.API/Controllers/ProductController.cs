014b29ecb2bb0d9810aa8951def4da075619256b
JhonnyPonce <jhonnymaikom@hotmail.com>
2022-12-27 20:21:37 -0300

    Finalizado controle de produtos.

---
+++ Empromel.API/Empromel.API/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Empromel.API.Models;
+using Empromel.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empromel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductsServices _productsServices;
+
+        public ProductController(ProductsServices productsServices)
+        {
+            this._productsServices = productsServices;
+        }
+
+        [Route("addproduct")]
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _productsServices.AddProduct(product);
+            return Ok();
+        }
+
+        [Route("getallproduct")]
+        [HttpGet]
+        public IActionResult GetAllProduct()
+        {
+            return Ok(_productsServices.GetAllProducts());
+        }
+
+        [Route("getproductbyname/{name}")]
+        [HttpGet]
+        public IActionResult GetProductByName(string name)
+        {
+            return Ok(_productsServices.GetProductByName(name));
+        }
+
+        [Route("updateproduct")]
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productsServices.UpdateProduct(product);
+            return Ok();
+        }
+
+        [Route("deleteproduct/{name}")]
+        [HttpDelete]
+        public IActionResult DeleteProduct(string name)
+        {
+            _productsServices.DeleteProduct(name);
+            return Ok();
+        }
+    }
+}

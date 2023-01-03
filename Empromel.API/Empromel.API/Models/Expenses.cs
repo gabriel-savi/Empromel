d0d5593f76c532c070dbcc7973b557db8e5708b5
JhonnyPonce <jhonnymaikom@hotmail.com>
2023-01-03 19:29:02 -0300

    Criado Model da tabela de despesas.

---
+++ Empromel.API/Empromel.API/Models/Expenses.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Empromel.API.Models
+{
+    public class Expenses
+    {
+        [Key]
+        public Guid Id { get; set; }    
+
+        [Required]
+        [MaxLength(100)]    
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required]
+        [MaxLength(7)]
+        public string PricePaid { get; set; }
+
+    }
+}

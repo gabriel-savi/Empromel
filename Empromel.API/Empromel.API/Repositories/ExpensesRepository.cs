6ef155dfdb24510d5b06fa00ee0f8c8e4ed6a450
JhonnyPonce <jhonnymaikom@hotmail.com>
2023-01-03 19:53:21 -0300

    Criado o repositorio das despesas.

---
+++ Empromel.API/Empromel.API/Repositories/ExpensesRepository.cs
@@ -0,0 +1,46 @@
+﻿using Empromel.API.Data.Context;
+using Empromel.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace Empromel.API.Repositories
+{
+    public class ExpensesRepository
+    {
+        private readonly DataContext _context;
+
+        public ExpensesRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void AddExpenses(Expenses expenses)
+        {
+            _context.Expenses.Add(expenses);
+            _context.SaveChanges();
+        }
+
+        public List<Expenses>GetAllExpenses()
+        {
+            return _context.Expenses.ToList();
+        }
+
+        public Expenses GetExpensesByDescription(string description)
+        {
+            return _context.Expenses.First(p => p.Description == description);
+        }
+
+        public void UpdateExpenses(Expenses expenses)
+        {
+            _context.Expenses.Update(expenses);
+            _context.SaveChanges();
+        }
+
+        public void DeleteExpenses(Expenses expenses)
+        {
+            _context.Expenses.Remove(expenses);
+            _context.SaveChanges();
+        }
+
+    }
+}

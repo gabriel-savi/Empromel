bc05c0796c0648e1cdb578f493431fc1097b3d31
jhonn <jhonn@DESKTOP-F05TF1N>
2023-03-20 19:59:49 -0300

    Vendas atualizada.

--- Empromel.API/Empromel.API/Repositories/SalesAndSalesItemsRepository.cs
+++ Empromel.API/Empromel.API/Repositories/SalesAndSalesItemsRepository.cs
@@ -11,24 +11,14 @@ namespace Empromel.API.Repositories
         public SalesAndSalesItemsRepository(DataContext context)
         {
             _context = context;
-        }   
+        }
 
         public void AddSalesAndSalesItems(Sales sales, SalesItems salesItems)
         {
-            using var transaction = _context.Database.BeginTransaction();
-            try
-            {
-                _context.Sales.Add(sales);
-                _context.SalesItems.Add(salesItems);
-                _context.SaveChanges();
+            _context.Sales.Add(sales);
+            _context.SalesItems.Add(salesItems);
+            _context.SaveChanges();
 
-                transaction.Commit();
-            }
-            catch (Exception)
-            {
-                transaction.Rollback();
-                throw;
-            }
         }
 
         public List<Sales> GetAllSalesAndSaleItems()
@@ -45,38 +35,18 @@ namespace Empromel.API.Repositories
 
         public void UpdateSalesAndSalesItems(Sales sales, SalesItems salesItems)
         {
-            using var transaction = _context.Database.BeginTransaction();
-            try
-            {
-                _context.Sales.Update(sales);
-                _context.SalesItems.Update(salesItems);
-                _context.SaveChanges();
+            _context.Sales.Update(sales);
+            _context.SalesItems.Update(salesItems);
+            _context.SaveChanges();
 
-                transaction.Commit();
-            }
-            catch (Exception)
-            {
-                transaction.Rollback();
-                throw;
-            }
         }
 
         public void DeleteSalesAndSalesItems(Sales sales, SalesItems salesItems)
         {
-            using var transaction = _context.Database.BeginTransaction();
-            try
-            {
-                _context.Sales.Remove(sales);
-                _context.SalesItems.Remove(salesItems);
-                _context.SaveChanges();
 
-                transaction.Commit();
-            }
-            catch (Exception)
-            {
-                transaction.Rollback();
-                throw;
-            }
+            _context.SalesItems.Remove(salesItems);
+            _context.Sales.Remove(sales);
+            _context.SaveChanges();
 
         }
 
---
+++ Empromel.API/Empromel.API/Services/SalesAndSalesItemsService.cs
@@ -0,0 +1,21 @@
+﻿using Empromel.API.Models;
+using Empromel.API.Repositories;
+
+namespace Empromel.API.Services
+{
+    public class SalesAndSalesItemsService
+    {
+        private readonly SalesAndSalesItemsRepository _repository;
+
+        public SalesAndSalesItemsService (SalesAndSalesItemsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddSalesAndSalesItems (Sales sales, SalesItems salesItems)
+        {
+            
+        }
+    }
+}
+
